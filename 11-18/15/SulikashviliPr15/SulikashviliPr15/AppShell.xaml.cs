537e0d4ccb2979e89ccfcb6fdb5c3034a9fc8ad3
Naddein <96597794+progulival-pary@users.noreply.github.com>
2022-05-22 17:46:44 +0300

    11-18
    11-18

---
+++ 11-18/15/SulikashviliPr15/SulikashviliPr15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using SulikashviliPr15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace SulikashviliPr15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ 11-18/16/WebSulikashviliPr16/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebSulikashviliPr16.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
